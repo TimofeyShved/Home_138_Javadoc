fa3f800fcf9c093f15b24ee3ab373645966251f9
TimofeyShved <timofeya123@inbox.ru>
2023-11-25 13:48:05 +0600

    work doc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,54 @@
 package com.company;
 
+/*
+* <h1>Это мой главный класс, я буду писать в нём код</h1>
+* @author Timofey
+* @version 0.0.1
+* @sine 1.0
+* В документацию можно вписывать как в <b>html</b>
+* <img src="doc-files/enot.jpeg" alt="enot" /?>
+* */
 public class Main {
 
-    public static void main(String[] args) {
-	
+    /*
+    *  Это моё поле и его зовут myField, он целочисленный
+    *
+    * */
+    int myField;
 
+    /*
+    * Это стартовая точка программы
+    * @param args
+    * */
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    * Этот метод вернёт i
+    * @param i целое число
+    * @param s строка
+    * @throws RuntimeException обработка ошибки
+    * */
+    int getMyField(int i, String s) throws RuntimeException{
+
+        return i;
+    }
+
+    /*
+    * Старый метод
+    * @deprecated Используйте вместо него новый метод "newMethod"
+    * */
+    void oldMethod(){
+        System.out.println("Устаревшие действия");
+    }
+
+    /*
+     * Новый метод
+     * @see OtherClass#otherMetod
+     * @link OtherClass#otherMetod
+     * */
+    void newMethod(){
+        System.out.println("Новые действия");
     }
 }
---
+++ src/com/company/OtherClass.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class OtherClass {
+    void otherMetod(){
+        System.out.println("Другой метод, в другом классе");
+    }
+}
